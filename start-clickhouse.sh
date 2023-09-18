e337afda40c9c3b64b0c140da748e74c97d4e1d9
AliALS9 <81914050+AliALS9@users.noreply.github.com>
2023-09-18 15:42:00 +0300

    Update start-clickhouse.sh

--- start-clickhouse.sh
+++ start-clickhouse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting ClickHouse"
-docker run --name clickhouse -d bitnami/clickhouse:latest -e ALLOW_EMPTY_PASSWORD='yes'
+docker run --name clickhouse -p 8123:8123 -d bitnami/clickhouse:latest -e ALLOW_EMPTY_PASSWORD='yes'
